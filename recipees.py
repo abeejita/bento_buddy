970874a51c5864980446dc36dfe5adc7231e8aed
abeejita <a01655892@tec.mx>
2025-01-08 03:53:48 -0600

    Finished Bento Buddy Project.

--- recipees.py
+++ recipees.py
@@ -2,10 +2,12 @@ from tkinter import *
 import os
 from dotenv import load_dotenv
 import requests
+from PIL import Image, ImageTk
+import io
 
 # Create the root window
 root = Tk()
-root.geometry('1024x768')
+root.geometry('1200x900')
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
@@ -49,6 +51,7 @@ def show_main_screen():
     ingredients_frame.pack(pady=10)
     find_button.pack(pady=10)
     recipe_frame.pack_forget()
+    recipe_card_frame.pack_forget()
 
 def show_recipe_screen():
     find_recipes()
@@ -60,10 +63,79 @@ def show_recipe_screen():
     ingredients_frame.pack_forget()
     find_button.pack_forget()
     recipe_frame.pack()
+    recipe_card_frame.pack_forget()
+
+def show_card_screen(recipe_id):
+    main_frame.pack_forget()
+    lookup_label.pack_forget()
+    Searchbar.pack_forget()
+    lookup_button.pack_forget()
+    result_frame.pack_forget()
+    ingredients_frame.pack_forget()
+    find_button.pack_forget()
+    recipe_card_frame.pack()
+    recipe_frame.pack_forget()
+
+    show_recipe_cards(recipe_id)
 
 def clear_frame(frame):
     for widget in frame.winfo_children():
         widget.destroy()
+
+def show_recipe_cards(recipe_id):
+    endpoint =  base_url + "/recipes/" + str(recipe_id) + "/card"
+    params = {
+        'apiKey': api_key
+    }
+
+    response = requests.get(endpoint, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        image_url = data["url"]
+
+        image_response = requests.get(image_url)
+        if image_response.status_code == 200:
+            image_data = image_response.content
+            image = Image.open(io.BytesIO(image_data))
+
+            original_width, original_height = image.size
+            aspect_ratio = 0.65
+
+            resized_image = image.resize((int(original_width * aspect_ratio), int(original_height * aspect_ratio)), Image.LANCZOS)
+            recipe_card_image = ImageTk.PhotoImage(resized_image)
+
+            clear_frame(recipe_card_frame)
+
+            top_frame = Frame(recipe_card_frame, bg = "white")
+            top_frame.pack(fill="x", pady=5)
+
+            back_button = Button(top_frame, text="Back", command=show_recipe_screen)
+            back_button.pack(side=LEFT, padx=5, pady=5)
+
+            recipe_card_label = Label(recipe_card_frame, image=recipe_card_image)
+            recipe_card_label.image = recipe_card_image
+            recipe_card_label.pack(side=LEFT, padx = 10, pady = 5)
+
+            nutrition_endpoint =  base_url + "/recipes/" + str(recipe_id) + "/nutritionLabel.png"
+            nutrition_response = requests.get(nutrition_endpoint, params=params)
+            if nutrition_response.status_code == 200:
+                nutrition_data = nutrition_response.content
+                nutritional_label_image = ImageTk.PhotoImage(Image.open(io.BytesIO(nutrition_data)))
+                
+                nutritional_label_label = Label(recipe_card_frame, image=nutritional_label_image)
+                nutritional_label_label.image = nutritional_label_image
+                nutritional_label_label.pack(side=LEFT, padx=10, pady=5)
+            else:
+                print("ERROR: " + str(nutrition_response.status_code))
+
+            recipe_card_frame.pack()
+        else:
+            result = "ERROR: " + str(response.status_code)
+            print(result)
+    else: 
+        result = "ERROR: " + str(response.status_code)
+        print(result)
 
 def show_recipes(recipes):
     clear_frame(recipe_frame)
@@ -78,8 +150,8 @@ def show_recipes(recipes):
     recipes_label.pack(pady=5)
 
     for recipe in recipes:
-        recipe_label = Label(recipe_frame, text=recipe["title"])
-        recipe_label.pack(pady=2)
+        recipe_button = Button(recipe_frame, text=recipe["title"], command=lambda recipe_id=recipe["id"]: show_card_screen(recipe_id))
+        recipe_button.pack(pady=5)
 
 def find_recipes():
     ingredients = ",".join(added_ingredients)
@@ -131,6 +203,15 @@ frame.pack(pady=10)
 main_frame = Frame(root)
 main_frame.pack()
 
+logo = Image.open("bento_buddy_logo.png")
+tk_logo = ImageTk.PhotoImage(logo)
+logo_label = Label(main_frame, image=tk_logo)
+logo_label.image = tk_logo
+logo_label.pack(pady=20)
+
+app_label = Label(main_frame, text="Bento Buddy", font=("Arial", 24, "bold"))
+app_label.pack(pady=10)
+
 lookup_label = Label(main_frame, text="Select the ingredients ", font=("Arial", 14, "bold"))
 lookup_label.pack(pady=5)
 
@@ -151,11 +232,14 @@ SearchResultBar.pack(side=LEFT, padx=5)
 ingredients_frame = Frame(root)
 ingredients_frame.pack(pady=10)
 
-find_button = Button(root, text="Find Recipees", command=show_recipe_screen)
+find_button = Button(root, text="Find Recipes", command=show_recipe_screen)
 find_button.pack(padx=5)
 
 added_ingredients = []
 
 recipe_frame = Frame(root)
+
+recipe_card_frame = Frame(root, bg = "white")
+recipe_card_frame.pack_forget()
 
 root.mainloop()
